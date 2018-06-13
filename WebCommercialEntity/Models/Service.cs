d471675faafa3036d2b8b2b518576bdf67acf075
Steevin <steevinamouzou@hotmail.fr>
2018-06-13 22:15:06 +0200

    updatebis

--- WebCommercialEntity/Models/Service.cs
+++ WebCommercialEntity/Models/Service.cs
@@ -72,24 +72,6 @@ namespace WebCommercialEntity.Models
             }
         }
 
-        public void AjouterClient(clientel c)
-        {
-            Serreurs er = new Serreurs("Erreur sur l'ajout d'un client",
-                 "Client.AjouterClient()");
-            try
-            {
-                unCommercial.clientel.Add(c);
-                unCommercial.SaveChanges();
-            }
-            catch (MonException e)
-            {
-                throw new MonException(er.MessageUtilisateur(),
-                    er.MessageApplication(), e.Message);
-            }
-        }
-
-
-
 
         ///
         ///  Classe Clientel
